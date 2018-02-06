5ba2dbaa24d32a7db87eb10be6bd117c323fd649
Jayden Latchuk <jaydenlatchuk@Jaydens-MacBook-Pro-2.local>
2018-02-05 18:43:34 -0700

    Uploading Project

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Track from '../Track/Track';
+import './Tracklist.css';
+
+
+class Tracklist extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {this.props.tracks.map(track => <Track key={track.id} track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />)}
+      </div>
+    );
+  }
+}
+
+export default Tracklist;
